09411dc9f11a955744c0b56b6ef3b0c1bbe3f34e
Casey Childers <childerskc@gmail.com>
2015-07-19 15:17:47 -0700

    Main AMVC Controller
      * Main AMVC Controller
      *
      * Extend your controllers off this controller.
      *
      * @author  Casey Childers <casey@axori.com>
      *
      * @version 1.0
      *
      * @param bool    			$IsRoute  Pass True to this param to trigger it. Use the defined Route's to route pages.
      * @param string|closure	$Function This is the name of the function on the object to trigger, or a closure function
      *
      * @example Page Controller
    		define('AboutUs','/^about-us\/?$/')
    		$Page = new Controller(AboutUs,function($p) {
    			return 'About Us';
    		});
    		
      * @example Permission Controller
    		$UserForm = new Controller($HasPermissionTo['Do Something'],function($p){
    			return 'User Form';
    		});

---
+++ include/Core/Controller.php
@@ -0,0 +1,58 @@
+<?php
+/**
+  * Main AMVC Controller
+  *
+  * Extend your controllers off this controller.
+  *
+  * @author  Casey Childers <casey@axori.com>
+  *
+  * @version 1.0
+  *
+  * @param bool    			$IsRoute  Pass True to this param to trigger it. Use the defined Route's to route pages.
+  * @param string|closure	$Function This is the name of the function on the object to trigger, or a closure function
+  *
+  * @example Page Controller
+		define('AboutUs','/^about-us\/?$/')
+		$Page = new Controller(AboutUs,function($p) {
+			return 'About Us';
+		});
+		
+  * @example Permission Controller
+		$UserForm = new Controller($HasPermissionTo['Do Something'],function($p){
+			return 'User Form';
+		});
+  */
+class Controller {
+	public $CSS = '';
+	public $JS = '';
+	
+	public function __construct($IsRoute,$Function = null) {
+		global $p;
+		
+		if($IsRoute) {
+			if(is_string($Function)) {
+				$this->Result = $this->$Function($p);
+			}
+			if(is_closure($Function)) {
+				$this->Result = $Function($p);
+			}
+			
+			if(isset($this->Result) && !is_string($this->Result)) {
+				$this->CSS .= $this->Result->CSS;
+				$this->JS .= $this->Result->JS;
+			}
+		}
+	}
+	
+	public function __toString() {
+		try {
+			if(isset($this->Result)) {
+				return (string)$this->Result;
+			}
+		} catch(Exception $e) {
+			die('Error: ' . $e->getMessage());
+		}
+		return '';
+	}
+}
+?>
