6496d930da84abfeffdd9bc42235677804086a08
Casey Childers <childerskc@gmail.com>
2015-07-25 16:42:19 -0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	/** Handle Required Root Defines **/
 	define('FILEROOT','include/');
 	define('COREROOT',FILEROOT.'Core/');
-	define('VIEWROOT',FILEROOT.'View/');
+	define('HTMLROOT',FILEROOT.'HTML/');
 	define('CSSROOT',FILEROOT.'CSS/');
 	define('JSROOT',FILEROOT.'JS/');
 
@@ -15,13 +15,12 @@
 	define("URL", "http://".$_SERVER['HTTP_HOST'].PROJECT);
 	
 	define('DBH','localhost');
-	define('DBN','name');
-	define('DBU','user');
-	define('DBP','pass');
+	define('DBN','Name');
+	define('DBU','User');
+	define('DBP','Pass');
 	
-	define('EncryptionPassword','SierraBankruptcy Admin');
+	define('EncryptionPassword','EncryptionPassword');
 	define('EncryptionLength','256');
-
 	
 	/** Include the AMVC Core **/
 	include(COREROOT.'Core.php');
@@ -37,85 +36,6 @@
 		'GetPage'	=> p('/^get\/(stuff|something)?\/?$/'),
 	]);
 	
-	/** Handle our Project **/
-	echo new Page(AllPages,function($p) {
-		/* Handle any logic that needs to happen on all pages at the Setup level */
-		
-		/* Return our Setup view */
-		return new View('Setup',[
-			'Base' => URL,
-			'Title' => 'AMVC: New Project',
-			'Keywords' => 'axori,mvc',
-			'Description' => 'This is a new project amvc description',
-			'FavIcon' => '',
-			
-			// Create a Body Controller and connect it to any set of pages
-			'Body' => new Controller(AllPages,function($p) {
-				/* Handle any logic that needs to happen on pages with this body controller */
-				$Header = 'Header Data';
-				$Footer = 'Footer Data';
-				
-				/* Switch based on an action, in this case the PAGE, this is the Body Controller's possible 'Actions' it can take, and is not limited to the PAGE, but can refer to items part of the $p, $_POST, $_GET, or anything that refers to an interaction from the user with this controller */
-				switch(PAGE) {
-					case Index:
-						$Body = new Controller(Index,function($p) { // Index Controller
-							$Example = '... Hi!';
-
-							// Return our main index view, using above data to output into the views
-							return new View('Page/Index',[
-								'Body' => $Example,
-							]);
-						});
-					break;
-					case AboutUs:
-						$Body = new Controller(AboutUs,function($p) { // About Us Controller
-							// Do some controller logic here, which is just processing the user events
-							$AboutWho = str_replace('about-','',$p[0]);
-							$AboutPage = '';
-
-							// Switch based on the different available actions for this controller
-							switch($AboutWho) {
-								case 'casey':
-									// return a model to the controller
-									$Employee = new Model\Select\Employee([
-										'Name' => 'Casey Childers',
-									]);
-
-									$AboutPage = $Employee->AddToView('Page/Employee');
-								break;
-								case 'chris':
-									$Employee = new Model\Select\Employee([
-										'Name' => 'Chris Childers',
-									]);
-									$AboutPage = $Employee->AddToView('Page/Employee');
-								break;
-								default:
-									$Employees = new Model\Select\Employees();
-									foreach($Employees->Result as $i => $Employee) {
-										$AboutPage .= new View('Page/Employees/Employee',$Employee);
-									}
-
-									$AboutPage = new View('Page/AboutUs',[
-										'Employees' => $AboutPage
-									]);
-								break;
-							}
-
-							// Return a string, the controller will output it
-							return $AboutPage;
-						});
-
-					break;
-				}
-				
-				/* Return our Body view */
-				return new View('Body',[
-					'Header' => $Header,
-					'Body' => $Body,
-					'Footer' => $Footer,
-				]);
-			}),
-		]);
-	});
-	
+	/** Setup our Project Code **/
+	include(FILEROOT.'Setup.php');	
 ?>
