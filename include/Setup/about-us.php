c6c6b4fec7a81b2c65d21e6e307ac27ab6f3f474
Casey Childers <childerskc@gmail.com>
2015-07-25 17:20:46 -0700

    Create about-us.php

---
+++ include/Setup/about-us.php
@@ -0,0 +1,18 @@
+<?php
+/** /Setup/about-us.php Controller is Included By Setup.php
+	Provides           (string)$BodyHeader  - {BodyHeader} Tag Replace Data
+	Provides & Expects (string)$BodyPage    - {BodyPage} Tag Replace Data
+	Provides           (string)$BodyFooter  - {BodyFooter} Tag Replace Data
+	Provides           (string)$PAGE        - Control & View File Name
+**/
+
+// Index PAGE Controller
+new Controller(AboutUs,function($p) use(&$Page,$PAGE) { // <--- Note the use()
+	$Example = '... Index Page..';
+
+	// Return our main index view, using above data to output into the views
+	$Page = new View('Body/'.$PAGE,[
+		'Data' => $Example,
+	]);
+});
+?>
